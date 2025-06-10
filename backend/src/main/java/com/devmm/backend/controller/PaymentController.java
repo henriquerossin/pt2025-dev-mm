12249391e8f637a4e76919ad49ec2bab0674592e
Henrique Rossin <pro.henriquerossin@gmail.com>
2025-06-09 21:21:26 -0300

    criação do service

--- backend/src/main/java/com/devmm/backend/controller/PaymentController.java
+++ backend/src/main/java/com/devmm/backend/controller/PaymentController.java
@@ -2,54 +2,27 @@ package com.devmm.backend.controller;
 
 import com.devmm.backend.dto.PaymentRequest;
 import com.devmm.backend.dto.PaymentResponse;
+import com.devmm.backend.service.PaymentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {
 
+    @Autowired
+    private PaymentService paymentService;
+
     @PostMapping
     public ResponseEntity<PaymentResponse> processPayment(@Valid @RequestBody PaymentRequest request) {
-        String nome = request.getNomeTitular();
-        String bandeira = detectaBandeira(request.getNumeroCartao());
-
-        PaymentResponse response = new PaymentResponse("SUCESSO", "Pagamento processado com sucesso!", new DadosRetorno(nome, bandeira));
-
+        PaymentResponse response = paymentService.process(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    // Um método privado que vai simular a detecção da bandeira do cartão.
-    private String detectaBandeira(String numeroCartao) {
-        if (numeroCartao.startsWith("4")) {
-            return "Visa";
-        } else if (numeroCartao.startsWith("5")) {
-            return "MasterCard";
-        } else {
-            return "Bandeira desconhecida";
-        }
-    }
-
-    // Uma classe auxiliar interna que vai montar os dados da resposta
-    static class DadosRetorno {
-        private String nomeTitular;
-        private String bandeira;
-
-        public DadosRetorno(String nomeTitular, String bandeira) {
-            this.nomeTitular = nomeTitular;
-            this.bandeira = bandeira;
-        }
-
-        public String getNomeTitular() {
-            return nomeTitular;
-        }
-
-        public String getBandeira() {
-            return bandeira;
-        }
     }
 
 }
